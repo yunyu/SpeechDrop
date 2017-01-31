b8674d3697ce941ac0cee9325524383c47400784
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2017-01-30 22:20:20 -0600

    Switch to async hints

--- frontend/js-orig/main.js
+++ frontend/js-orig/main.js
@@ -1,26 +1,40 @@
-var produceHint = function (value) {
-    var codeInputBox = document.getElementById("code-input-box");
-    if (value.length < maxChars) {
-        codeInputBox.className = "";
-        var remaining = maxChars - value.length;
-        return remaining + " character" + (remaining > 1 ? "s" : "") + " left";
-    }
-    var http = new XMLHttpRequest();
-    http.open('GET', window.location.href + value + "/index", false);
-    http.send();
-    if (http.status != 200) {
-        codeInputBox.className = "input-invalid";
-        return '<span style="color:#b72a2a">Invalid room code</span>';
-    } else {
-        window.location.href += value;
-        return "Taking you there...";
-    }
-};
-
 var maxChars = 6;
+var codeInputBox = document.getElementById("code-input-box");
+var charRemaining = function (val) {
+    var remaining = maxChars - val.length;
+    return remaining + " character" + (remaining != 1 ? "s" : "") + " left";
+};
 var codeInput = new Vue({
     el: '#code-input',
     data: {
         code: '',
+        hint: charRemaining('')
     },
+    watch: {
+        code: function (val) {
+            this.produceHint(val);
+        }
+    },
+    methods: {
+        produceHint: function (val) {
+            if (val.length < maxChars) {
+                codeInputBox.className = "";
+                this.hint = charRemaining(val);
+            } else {
+                this.hint = "Checking...";
+                var http = new XMLHttpRequest();
+                http.open('GET', window.location.href + val + "/index");
+                http.onloadend = function () {
+                    if (http.status != 200) {
+                        codeInputBox.className = "input-invalid";
+                        codeInput.hint = '<span style="color:#b72a2a">Invalid room code</span>';
+                    } else {
+                        window.location.href += val;
+                        codeInput.hint = "Taking you there...";
+                    }
+                };
+                http.send();
+            }
+        }
+    }
 });

47d306cda15282c04db81ac9dd24c4e36623a0bc
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2017-07-11 08:21:21 -0600

    Bind to set host

--- src/main/java/edu/vanderbilt/yunyulin/speechdrop/SpeechDropVerticle.java
+++ src/main/java/edu/vanderbilt/yunyulin/speechdrop/SpeechDropVerticle.java
@@ -7,11 +7,11 @@ import io.vertx.ext.web.Router;
 import io.vertx.ext.web.impl.Utils;
 
 public class SpeechDropVerticle extends AbstractVerticle {
-    public static String LOCALHOST = "127.0.0.1";
+    public static String HTTP_HOST = "127.0.0.1";
     public static int HTTP_PORT = 6969;
 
     public static String VERSION;
-    public static int TWO_MONTHS = 60 * 24 * 60 * 60;
+    public static int PURGE_INTERVAL = 60 * 24 * 60 * 60;
 
     @Override
     public void start() throws Exception {
@@ -27,6 +27,6 @@ public class SpeechDropVerticle extends AbstractVerticle {
                 Utils.readFileToString(vertx, "about.html")
         ).mount(router);
 
-        httpServer.requestHandler(router::accept).listen(HTTP_PORT);
+        httpServer.requestHandler(router::accept).listen(HTTP_PORT, HTTP_HOST);
     }
 }

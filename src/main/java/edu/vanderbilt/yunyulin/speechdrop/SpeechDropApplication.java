a299db62309ffd10130c1d9715eb96895296601f
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2017-07-11 20:46:16 -0600

    Drop session storage to 6hr

--- src/main/java/edu/vanderbilt/yunyulin/speechdrop/SpeechDropApplication.java
+++ src/main/java/edu/vanderbilt/yunyulin/speechdrop/SpeechDropApplication.java
@@ -101,7 +101,7 @@ public class SpeechDropApplication {
         router.route().handler(CookieHandler.create());
         router.route().handler(SessionHandler.create(
                 LocalSessionStore.create(vertx, "speechdrop-sessions", 10000L)
-        ).setSessionTimeout(48 * 60 * 60 * 1000));
+        ).setSessionTimeout(6 * 60 * 60 * 1000));
         router.route().handler(CSRFHandler.create(config.getString("csrfSecret")));
 
         router.route("/").method(GET).handler(ctx ->

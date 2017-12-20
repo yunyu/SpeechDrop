d18fd4fd18ff9d4d8c8d49e195141bcb7d0c72f1
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2017-12-19 22:19:26 -0500

    Big frontend refactor

--- frontend/js-orig/room.js
+++ frontend/js-orig/room.js
@@ -1,50 +1,29 @@
-function formatDate(timestamp) {
-    var date = new Date(timestamp);
-    var hours = date.getHours();
-    var minutes = date.getMinutes();
-    var ampm = hours >= 12 ? 'pm' : 'am';
-    hours = hours % 12;
-    hours = hours ? hours : 12;
-    minutes = minutes < 10 ? '0' + minutes : minutes;
-    return hours + ':' + minutes + ' ' + ampm;
-}
-
-function processFileList(newList) {
-    var processed = [];
-    for (var i = 0; i < newList.length; i++) {
-        var currEl = newList[i];
-        if (!currEl) continue;
-        currEl.url = mediaUrl + roomId + '/' + i + '/' + currEl.name;
-        currEl.origPos = i;
-        currEl.formattedDate = formatDate(currEl.ctime);
-        processed.unshift(currEl);
-    }
-    return processed;
-}
-
-function getCsrfToken() {
-    return Cookies.get('XSRF-TOKEN');
-}
-
-// Token refresh
-setInterval(function () {
-    var r = new XMLHttpRequest();
-    r.open("GET", "/" + roomId + "/index", true);
-    r.send();
-}, 600000);
-
-var uploadedFiles = new Vue({
+new Vue({
     el: '#room-container',
     data: {
-        fileList: processFileList(initialFiles)
+        files: initialFiles
     },
     mounted: function () {
+        var self = this;
         ga('send', 'event', 'Room', 'join', roomId);
+        // Nasty workaround for https://github.com/vuejs/vue/issues/5800
+        var pendingAdd = false;
+
+        // Token refresh
+        setInterval(function () {
+            var r = new XMLHttpRequest();
+            r.open("GET", "/" + roomId + "/index", true);
+            r.send();
+        }, 600000);
 
         var eb = new EventBus('/sock');
         eb.onopen = function () {
             eb.registerHandler("speechdrop.room." + roomId, function (e, m) {
-                uploadedFiles.fileList = processFileList(JSON.parse(m.body));
+                if (pendingAdd) {
+                    pendingAdd = false;
+                } else {
+                    self.files = JSON.parse(m.body);
+                }
             });
         };
         eb.enableReconnect(true);
@@ -58,7 +37,7 @@ var uploadedFiles = new Vue({
             dropzoneElement.className = "dz-clickable";
         }
 
-        this.$nextTick(function () {
+        self.$nextTick(function () {
             var dropCard = new Dropzone("div#file-dropzone", {
                 url: "/" + roomId + "/upload",
                 addedfile: function (file) {
@@ -70,7 +49,12 @@ var uploadedFiles = new Vue({
                     resetDropzone(dropCard.element);
                     setUploadText(dropCard.element, "Upload successful!");
                     dropCard.element.className += " dropzone-success";
-                    uploadedFiles.fileList = processFileList(successMsg);
+                    // Avoid Vue bug: https://github.com/vuejs/vue/issues/5800
+                    pendingAdd = true;
+                    self.files = successMsg;
+                    setTimeout(function () {
+                        pendingAdd = false;
+                    }, 300);
                     setTimeout(function () {
                         resetDropzone(dropCard.element);
                     }, 2000);
@@ -85,7 +69,7 @@ var uploadedFiles = new Vue({
                     }, 2000);
                 },
                 sending: function (file, xhr, formData) {
-                    formData.append("X-XSRF-TOKEN", getCsrfToken());
+                    formData.append("X-XSRF-TOKEN", self.getCsrfToken());
                     ga('send', 'event', 'Room', 'upload', roomId);
                 },
                 createImageThumbnails: false,
@@ -98,42 +82,51 @@ var uploadedFiles = new Vue({
     },
     methods: {
         deleteFile: function (fileIndex) {
+            var self = this;
             var r = new XMLHttpRequest();
             r.open("POST", "/" + roomId + "/delete", true);
-            for (var i = 0; i < uploadedFiles.fileList.length; i++) {
-                if (uploadedFiles.fileList[i].origPos === fileIndex) {
-                    uploadedFiles.fileList.splice(i, 1);
+            for (var i = 0; i < self.fileList.length; i++) {
+                if (self.fileList[i].origPos === fileIndex) {
+                    self.files.splice(i, 1);
                     break;
                 }
             }
             r.onload = function () {
-                processFileList(JSON.parse(r.response));
+                self.files = JSON.parse(r.response);
             };
             var data = "fileIndex=" + fileIndex;
             r.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-            r.setRequestHeader('X-XSRF-TOKEN', getCsrfToken());
+            r.setRequestHeader('X-XSRF-TOKEN', self.getCsrfToken());
             r.send(data);
             ga('send', 'event', 'Room', 'delete', roomId);
+        },
+        getCsrfToken: function () {
+            return Cookies.get('XSRF-TOKEN');
+        }
+    },
+    computed: {
+        fileList: function () {
+            function formatDate(timestamp) {
+                var date = new Date(timestamp);
+                var hours = date.getHours();
+                var minutes = date.getMinutes();
+                var ampm = hours >= 12 ? 'pm' : 'am';
+                hours = hours % 12;
+                hours = hours ? hours : 12;
+                minutes = minutes < 10 ? '0' + minutes : minutes;
+                return hours + ':' + minutes + ' ' + ampm;
+            }
+
+            var processed = [];
+            for (var i = 0; i < this.files.length; i++) {
+                var currEl = this.files[i];
+                if (!currEl) continue;
+                currEl.url = mediaUrl + roomId + '/' + i + '/' + currEl.name;
+                currEl.origPos = i;
+                currEl.formattedDate = formatDate(currEl.ctime);
+                processed.unshift(currEl);
+            }
+            return processed;
         }
     }
 });
-
-/*
- var refreshCards = function () {
- var r = new XMLHttpRequest();
- r.open("GET", "/" + roomId + "/index?_=" + Date.now(), true);
- r.onload = function (e) {
- if (r.readyState === 4) {
- if (r.status === 200) {
- uploadedFiles.fileList = processFileList(JSON.parse(r.responseText));
- } else {
- console.error(r.statusText);
- }
- }
- };
- r.send(null);
- };
-
- refreshCards();
- setInterval(refreshCards, 3000);
- */
